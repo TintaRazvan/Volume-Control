2fbd6c390fab304a622761ba985b6d5f2a6b1360
TintaRazvan <antercode@yahoo.com>
2025-09-17 17:07:23 +0300

    finished the project

--- venv/volume_control.py
+++ venv/volume_control.py
@@ -8,6 +8,7 @@ my_hands= mp.solutions.hands.Hands()
 drawing_utils=mp.solutions.drawing_utils
 while True:
     _ , image=webcam.read()
+    image=cv2.flip(image,1)
     frame_height, frame_width,_ = image.shape
     rgb_image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     output=my_hands.process(rgb_image)
@@ -27,12 +28,12 @@ while True:
                     cv2.circle(img=image,center=(x,y),radius=8,color=(0,0,255),thickness=3)
                     x2=x
                     y2=y
-                dist=((x2-x1)**2+(y2-y1)**2)**(0.5)//4
-                cv2.line(image,(x1,y1),(x2,y2),color=(0,255,0),thickness=3)
-                if dist>50:
-                    pyautogui.press('volumeup')
-                else:
-                    pyautogui.press('volumedown')
+        dist=((x2-x1)**2+(y2-y1)**2)**(0.5)//4
+        cv2.line(image,(x1,y1),(x2,y2),color=(0,255,0),thickness=3)
+        if dist>50:
+                pyautogui.press('volumeup')
+        else:
+                pyautogui.press('volumedown')
 
 
 
